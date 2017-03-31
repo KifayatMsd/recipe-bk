f3707f2f9280dcbc4d9e3112f0ca42062bd53ab7
KifayatMsd <kifayat.1234u@gmail.com>
2017-03-31 19:47:45 +0500

    Upload

--- src/app/recipes/recipe-list/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Recipe } from '../recipe';
+
 @Component({
   selector: 'rb-recipe-item',
-  templateUrl: './recipe-item.component.html'
+  templateUrl: './recipe-item.component.html',
+  inputs: ['recipe']                         // The input array means it is set outside of this component
 })
 export class RecipeItemComponent implements OnInit {
-    @Input() recipe: Recipe;                // @Input() : Bcz it is set outside of this component
     recipeId: number;
-
-    constructor() { } 
+    constructor() { }
     ngOnInit() {
   }
 }
